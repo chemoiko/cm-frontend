ceefcb0aabf611c4aa01ba1b21c6a7f2f0892ee1
eli <elijahmurket@gmail.com>
2024-10-13 23:46:21 +0300

    initial

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -14,6 +14,7 @@ import cloudinary
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -100,6 +101,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 
 # Password validation
